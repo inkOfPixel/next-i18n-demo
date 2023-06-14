b2185a80ca727ec889885ef4aa14ca1196ef40f8
Massimo De Marchi <massimo@inkofpixel.com>
2023-06-14 11:50:13 +0200

    add typescript messages validation

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import { useTranslations } from "next-intl";
 
-export default function Index() {
+export default function IndexPage() {
   const t = useTranslations("IndexPage");
   return <h1>{t("title")}</h1>;
 }
