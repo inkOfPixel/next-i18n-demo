1cd52262d27c643966fe7995eae5a1dc07736ea9
Massimo De Marchi <massimo@inkofpixel.com>
2023-06-15 09:25:05 +0200

    use english as string

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,15 +1,29 @@
 import { useTranslations } from "next-intl";
 
 export default function IndexPage() {
-  const t = useTranslations("IndexPage");
+  const t = useTranslations();
+  let description = t.rich(
+    "This is a basic example that demonstrates the usage of <code>next-intl</code> with the Next<dot/>js App Router<dot/> Try changing the locale in the top right corner and see how the content changes<dot/>",
+    {
+      code: (children) => <code>{children}</code>,
+    }
+  );
   return (
     <div>
-      <h1>{t("title")}</h1>
-      <div>
-        {t.rich("description", {
-          code: (children) => <code>{children}</code>,
-        })}
-      </div>
+      <h1>{t("next-intl example")}</h1>
+      {typeof description === "string" ? (
+        <div
+          dangerouslySetInnerHTML={{
+            __html: replaceDot(description),
+          }}
+        ></div>
+      ) : (
+        <div>{description}</div>
+      )}
     </div>
   );
+}
+
+function replaceDot(fallback: string) {
+  return fallback.replaceAll("<dot/>", ".");
 }
