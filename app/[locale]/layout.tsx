685d9641b20646f58d52d0cfe679fcf16329b00d
Massimo De Marchi <massimo@inkofpixel.com>
2023-06-16 17:26:36 +0200

    add navigation feedback

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import { useLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import Link from "next-intl/link";
+import { classNames } from "@/helpers/style-helpers";
 
 export default function LocaleLayout({ children, params }: any) {
   const locale = useLocale();
@@ -13,23 +14,38 @@ export default function LocaleLayout({ children, params }: any) {
   return (
     <html lang={locale}>
       <body>
-        <ul className="flex space-x-2 bg-gray-900 h-16 px-5 items-center justify-end">
-          <li className="hover:underline">
-            <Link href="/" locale="en">
-              English
-            </Link>
-          </li>
-          <li className="hover:underline">
-            <Link href="/" locale="it">
-              Italian
-            </Link>
-          </li>
-          <li className="hover:underline">
-            <Link href="/" locale="de">
-              German
-            </Link>
-          </li>
-        </ul>
+        <div className="flex bg-gray-900 h-16 px-5  justify-end">
+          <ul className="flex space-x-2 h-full items-center">
+            <li className="hover:underline">
+              <Link
+                href="/"
+                locale="en"
+                className={classNames(locale === "en" && "underline")}
+              >
+                English
+              </Link>
+            </li>
+            <li className="hover:underline">
+              <Link
+                href="/"
+                locale="it"
+                className={classNames(locale === "it" && "underline")}
+              >
+                Italian
+              </Link>
+            </li>
+            <li className="hover:underline">
+              <Link
+                href="/"
+                locale="de"
+                className={classNames(locale === "de" && "underline")}
+              >
+                German
+              </Link>
+            </li>
+          </ul>
+        </div>
+
         {children}
       </body>
     </html>
